fe6e578fcfc05a19ff7efe700385e5cd55bc9d0e
Sohan <sohan.m.14911@gmail.com>
2026-02-14 17:01:07 +0530

    Added toasts container and local fonts

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-
-import AppRoutes from "./routes";
-import theme from "./theme";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import AppRoutes from "./routes";
+import theme from "./theme";
+import "./index.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -16,6 +18,14 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <AppRoutes />
+          <ToastContainer
+            position="top-right"
+            autoClose={3000}
+            newestOnTop
+            closeOnClick
+            pauseOnHover
+            draggable
+          />
         </ThemeProvider>
       </Provider>
     </BrowserRouter>
