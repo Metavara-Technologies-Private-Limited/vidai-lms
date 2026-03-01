c2de69af4898e2be531f3e70f3640a8e37bebe5e
ANIL KUAMR REDDY N <nareanilreddy@outlook.com>
2026-03-01 20:53:30 +0530

    Referral management Dashboard changes with UI with mockData

--- src/components/Referrals/Zoya.tsx
+++ src/components/Referrals/Zoya.tsx
@@ -14,49 +14,118 @@ import {
 } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import { zoyaMock } from "./zoya.mock";
+import BackwardIcon from "../../assets/icons/Backward_icon.svg";
+import {useNavigate} from "react-router-dom";
 
-const ZoyaList: React.FC = () => {
+const Zoya: React.FC = () => {
   const [search, setSearch] = useState("");
+  const navigate = useNavigate();
 
   const filteredData = zoyaMock.filter((item) =>
     item.partnerName.toLowerCase().includes(search.toLowerCase())
   );
 
   return (
-    <Box p={3}>
-      <Typography variant="h5" fontWeight={600} mb={3}>
+  <Box p={1}>
+  {/* Header Row */}
+  <Box
+    display="flex"
+    alignItems="center"
+    justifyContent="space-between"
+    mb={3}
+    mt={-2}
+  >
+    {/* Left Side */}
+    <Box display="flex" alignItems="center" gap={2}>
+      <Box
+        component="img"
+        src={BackwardIcon}
+        alt="Back"
+        sx={{
+          width: 40,
+          height: 40,
+          cursor: "pointer",
+        }}
+        onClick={() => navigate(-1)}
+      />
+
+      <Typography variant="h6" fontWeight={600} mt={-1}>
         Zoya Partners ({filteredData.length})
       </Typography>
+    </Box>
 
-      <Box display="flex" justifyContent="flex-end" mb={2}>
-        <TextField
-          placeholder="Search by Partner name"
-          size="small"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon />
-              </InputAdornment>
-            ),
-          }}
-        />
-      </Box>
+    {/* Right Side (Search moved up) */}
+    <TextField
+      placeholder="Search by Partner name"
+      value={search}
+      onChange={(e) => setSearch(e.target.value)}
+      sx={{
+        width: 300,
+        "& .MuiOutlinedInput-root": {
+          height: 38,
+        },
+        "& input::placeholder": {
+          fontSize: "14px",
+          color: "#9E9E9E",
+          opacity: 1,
+        },
+      }}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <SearchIcon sx={{ fontSize: 20, color: "#9E9E9E" }} />
+          </InputAdornment>
+        ),
+      }}
+    />
+  </Box>
 
-      <Paper elevation={0} sx={{ borderRadius: 3, overflow: "hidden" }}>
-        <Table>
-          <TableHead sx={{ backgroundColor: "#f7f7f7" }}>
-            <TableRow>
-              <TableCell><b>Partner Name</b></TableCell>
-              <TableCell><b>Email | Contact</b></TableCell>
-              <TableCell><b>Referrals</b></TableCell>
-              <TableCell><b>Region</b></TableCell>
+      <Paper elevation={0} sx={{  overflow: "hidden" }}>
+        <Table sx={{ borderCollapse: "separate" }}>
+<TableHead
+  sx={{
+    "& .MuiTableCell-head": {
+      backgroundColor: "#f8f8f9",
+      color: "#626262",
+      fontSize: 14,
+      borderBottom: "none",
+      paddingY: 1,
+    },
+    // Top Left
+    "& .MuiTableCell-head:first-of-type": {
+      borderTopLeftRadius: "12px",
+      borderBottomLeftRadius: "12px",
+    },
+    // Top Right
+    "& .MuiTableCell-head:last-of-type": {
+      borderTopRightRadius: "12px",
+      borderBottomRightRadius: "12px",
+    },
+  }}
+>
+
+              <TableRow>
+              <TableCell>Partner Name</TableCell>
+              <TableCell>Email | Contact</TableCell>
+              <TableCell>Referrals</TableCell>
+              <TableCell>Region</TableCell>
             </TableRow>
           </TableHead>
 
-          <TableBody>
-            {filteredData.map((item) => (
+<TableBody
+  sx={{
+    "& .MuiTableCell-body": {
+      color: "#232323",
+      fontSize: "13px",
+      py: 1.5,
+    },
+        "& .MuiTableCell-body .MuiTypography-root": {
+      color: "#232323",
+      fontSize: "13px",
+    },
+  
+  }}
+>            {filteredData.map((item) => (
               <TableRow key={item.id} hover>
                 <TableCell>
                   <Box display="flex" alignItems="center" gap={2}>
@@ -69,7 +138,7 @@ const ZoyaList: React.FC = () => {
 
                 <TableCell>
                   <Typography>{item.email}</Typography>
-                  <Typography variant="body2" color="text.secondary">
+                  <Typography variant="body2" sx={{ mt: 0.5 }}>
                     {item.phone}
                   </Typography>
                 </TableCell>
@@ -86,4 +155,4 @@ const ZoyaList: React.FC = () => {
   );
 };
 
-export default ZoyaList;
+export default Zoya;
